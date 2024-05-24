be788daf0587ade65922153aaed08b8ef0bc4539
bshres77 <bijalshrestah77@gmail.com>
2024-05-23 19:34:52 -0500

    New tele op

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MyTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MyTeleOp.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+
+@TeleOp(group = "TeleOp", name = "TestTeleOp")
 public class MyTeleOp extends OpMode {
     @Override
     public void init() {
